9ae85d43136a80e211d70772c075a847509308b5
João Azevedo <59920486+joaoazevedoJS@users.noreply.github.com>
2020-03-14 14:49:27 -0300

    Add nunjucks

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ const express = require('express')
 const bodyParse = require("body-parser")
 const nodemailer = require('nodemailer')
 const config = require("./config/emailConfig.json")
+const nunjucks = require('nunjucks')
 
 const app = express()
 
 app.use(express.json())
+app.use(express.static('public'))
 app.use(bodyParse.urlencoded({ extended: true }))
+
+nunjucks.configure('./', {
+    express: app,
+    noCache: true,
+})
+
+app.get('/', (req, res) => {
+    res.render('./public/index.html')
+})
 
 app.post('/', (req, res) => {
     const { email, subject, message } = req.body
@@ -27,14 +38,9 @@ app.post('/', (req, res) => {
         text: `${message}\n\n\n\n Date ${new Date()}`
     }
 
-    transporter.sendMail(SendMessage, function (error, info) {
-        if (error) {
-            return res.json(error)
-        }
-        else {
-            return res.json(`Email enviado: ${info.response}`)
-        }
-    })
+    transporter.sendMail(SendMessage)
+
+    return res.redirect('/')
 })
 
-app.listen(8080, () => console.log(`Server iniciado... https://localhost:8080`))
+app.listen(8080, () => console.log(`Server iniciado... http://localhost:8080`))
